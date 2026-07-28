26871db81c8a5838e704f56c27365934804cf54a
PeaseScotty <connect@peasefulcleaningmoving.com>
2026-07-28 06:19:06 -0600

    Update business name in site-config.js

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,5 +1,5 @@
 window.SITE_CONFIG = {
-  publicBusinessName: 'Peaseful Cleaning N Moving',
+  publicBusinessName: 'Peaseful Cleaning & Moving',
   legalBusinessName: '',
   phoneDisplay: '(406) 258-8558',
   phoneLink: 'tel:+14062588558',
